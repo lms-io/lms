b2bd60118087d9571b7cec7b0c51cc39d3a56021
gavin <gavin@gavinm.com>
2015-09-08 23:55:58 -0400

    first go with cassandra works

--- src/api.py
+++ src/api.py
@@ -1,9 +1,25 @@
 from bottle import route,request 
+from cassandra.cluster import Cluster
+
 import configparser, thread, requests, sys, jsonpickle, random, os, zipfile, shutil 
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-@route('/sys/')
+cluster = Cluster()
+session = cluster.connect('demo')
+
+def from_json(arg):
+    return jsonpickle.decode(arg)
+
+def to_json(arg):
+    return jsonpickle.encode(arg,unpicklable=False) 
+
+@route('/sys')
 def sys_hello():
-    return "0.1" 
+    rows = session.execute('SELECT lastname, firstname, age FROM users')
+    d = [] 
+    for r in rows:
+        d.insert(0,{'lastname':r.lastname,'firstname':r.firstname,'age':r.age})
+    return to_json(d) 
+
