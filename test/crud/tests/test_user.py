9fee8bd27d041e17317d1bb047497d99a33f8623
gavin <gavin@gavinm.com>
2015-09-13 22:32:43 -0400

    more assertions, turning tests into tests

--- test/crud/tests/test_user.py
+++ test/crud/tests/test_user.py
@@ -1,5 +1,5 @@
 from cassandra.cluster import Cluster
-import random, os
+import random, os, bcrypt
 
 ks = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(16))
 
@@ -21,21 +21,14 @@ def test_insert():
           out = f.read()
           session.execute(out)
 
-  session.execute("""
-  INSERT INTO user (username, password) VALUES ('joe', 'bcrypt');
-          """)
+  ins = "INSERT INTO user (username, password) VALUES ('%s', '%s');"
+  session.execute(ins % ('joe', bcrypt.hashpw('bcrypt', bcrypt.gensalt())))
+  session.execute(ins % ('larry', bcrypt.hashpw('bcrypt', bcrypt.gensalt())))
+  session.execute(ins % ('moe', bcrypt.hashpw('bcrypt', bcrypt.gensalt())))
 
-  session.execute("""
-  INSERT INTO user (username, password) VALUES ('frank', 'bcrypt');
-          """)
-
-  session.execute("""
-  INSERT INTO user (username, password) VALUES ('larry', 'bcrypt');
-          """)
   rows = session.execute('SELECT username, password FROM user')
   d = [] 
   for r in rows:
-      d.insert(0,{'username':r.username,'password':r.password})
-  print d
+    assert r.password == bcrypt.hashpw('bcrypt'.encode('utf-8'), r.password.encode('utf-8'))
 
 
