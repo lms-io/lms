d01a827e55a042ebc517027727d97c0a550539c2
gavin <gavin@gavinm.com>
2015-09-15 00:33:45 -0400

    wrapping functional tests with a decorator

--- test/functional/happy.py
+++ test/functional/happy.py
@@ -1,39 +1,41 @@
 from cassandra.cluster import Cluster
 import random, os, uuid, bcrypt, setup 
 
-setup.start()
+@setup.cassandra
+def main():
+  cluster = Cluster()
+  session = cluster.connect()
+  ks = "lms"
+  kscql = """
+  CREATE KEYSPACE %s 
+  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+  """ % (ks)
+  session.execute(kscql)
 
-cluster = Cluster()
-session = cluster.connect()
-ks = "functionaltest"
-kscql = """
-CREATE KEYSPACE %s 
-WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
-""" % (ks)
-session.execute(kscql)
+  session = cluster.connect(ks)
+  organization = uuid.uuid1()
 
-session = cluster.connect(ks)
-organization = uuid.uuid1()
+  for name in os.listdir("cassandra"):
+    if name.endswith(".cql"):
+      with open("cassandra/" + name) as f:
+        out = f.read()
+        session.execute(out)
 
-for name in os.listdir("cassandra"):
-  if name.endswith(".cql"):
-    with open("cassandra/" + name) as f:
-      out = f.read()
-      session.execute(out)
+  insert = "insert into interaction (organization, id, url) values (%s, %s, %s)"
+  session.execute(insert, (organization, uuid.uuid1(), "http://google.com/?q=abc1"))
+  session.execute(insert, (organization, uuid.uuid1(), "http://google.com/?q=abc2"))
+  session.execute(insert, (organization, uuid.uuid1(), "http://google.com/?q=abc3"))
 
-insert = "insert into interaction (organization, id, url) values (%s, %s, %s)"
-session.execute(insert, (organization, uuid.uuid1(), "http://google.com/?q=abc1"))
-session.execute(insert, (organization, uuid.uuid1(), "http://google.com/?q=abc2"))
-session.execute(insert, (organization, uuid.uuid1(), "http://google.com/?q=abc3"))
+  rows = session.execute('SELECT organization, id, url FROM interaction')
+  d = []
+  for r in rows:
+    d.insert(0,str(r.id))
 
-rows = session.execute('SELECT organization, id, url FROM interaction')
-d = []
-for r in rows:
-  d.insert(0,str(r.id))
+  insert = "insert into campaign (organization, id, type, interactions) values (%s,%s,%s,%s)"
+  session.execute(insert, (organization, uuid.uuid1(), 'type', set(d)))
 
-insert = "insert into campaign (organization, id, type, interactions) values (%s,%s,%s,%s)"
-session.execute(insert, (organization, uuid.uuid1(), 'type', set(d)))
+  ins = "INSERT INTO user (organization, username, password) VALUES (%s,%s,%s);"
+  session.execute(ins, (organization, 'admin', bcrypt.hashpw('password', bcrypt.gensalt())))
+  print "data loaded, use username <admin> password <password>"
 
 
-raw_input("Data Loaded Press enter to close...")
-setup.stop()
--- test/functional/setup.py
+++ test/functional/setup.py
@@ -4,15 +4,21 @@ from ccmlib.cluster import Cluster
 CLUSTER_PATH="."
 cluster = Cluster(CLUSTER_PATH, 'functional-scenario', cassandra_version='2.2.1')
 
-def start():
+def cassandra(func):
   print("** STARTING **")
   cluster.populate(1).start()
   [node1] = cluster.nodelist()
   cluster.flush()
+  try:
+    func()
+  except:
+    print("failed to run functional test... ABORTING")
+    cluster.remove()
+    raise
 
-
-
-def stop():
+  raw_input("Data Loaded Press enter to close...")
   cluster.remove()
+
+
 
 
