e164bbe3d0f24e367b838d69e557660570933374
gavin <gavin@gavinm.com>
2015-09-22 22:28:09 -0400

    session management unit tests

--- src/api.py
+++ src/api.py
@@ -1,18 +1,66 @@
 from bottle import route,request 
 from cassandra.cluster import Cluster
 
-import thread, requests, sys, jsonpickle, random, os, zipfile, shutil 
+import uuid, traceback, redis, thread, bcrypt, configparser, requests, sys, jsonpickle, random, os, zipfile, shutil 
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+rhost = config.get('redis','host')
+rport = config.getint('redis','port')
+rdb = config.getint('redis','db')
+rdis = redis.StrictRedis(host=rhost, port=rport, db=rdb)
+syskey = config.get('application','syskey')
 
 keyspace = 'lms'
-
 def session():
   return Cluster().connect(keyspace)
+
+def callback(r, v):
+    if r.query.get('callback') is not None:
+        return '%s(%s)' % (r.query.get('callback'),v)
+    else:
+        return v
 
 def from_json(arg):
   return jsonpickle.decode(arg)
 
 def to_json(arg):
   return jsonpickle.encode(arg,unpicklable=False) 
+
+@route('/version')
+def sys_version():
+  rdis.set('version','0.1.0')
+  return callback(request,{'version':rdis.get('version')}) 
+
+@route('/auth/status')
+def auth_status():
+  return callback(request,{'status':''})
+
+@route('/auth/login', method='POST')
+def auth_login():
+  try:
+    username = request.forms.get('username') 
+    password = request.forms.get('password') 
+    organization = uuid.UUID(request.forms.get('organization')) 
+
+    if username is None or password is None:
+      return callback(request,{'status':'INVALID'})
+
+    usr = session().execute('SELECT username, password from user where username=%s and organization=%s', (username,organization))[0]
+    match = usr.password == bcrypt.hashpw(password.encode('utf-8'), usr.password.encode('utf-8'))
+    if match:
+      key = "%s:%s" % (organization, uuid.uuid1())
+      rdis.setex(key,2700,username)
+      return callback(request,{'status':'SUCCESSFUL', 'session':key})
+    return callback(request,{'status':'ERROR'})
+  except Exception, e:
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+    return callback(request,{'status':'EXCEPTION', 'message':lines})
+
+@route('/auth/logout')
+def auth_login():
+  return callback(request,{'status':'OK'})
 
 @route('/sys')
 def sys_hello():
--- test/test_user.py
+++ test/test_user.py
@@ -3,25 +3,37 @@ import random, os, uuid, bcrypt, sys, inspect, setup
 from cassandra.cluster import Cluster
 from webtest import TestApp
 
-ks = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(16))
 
 def test_insert():
+  ks = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(16))
   session = setup.keyspace(ks)
   app = setup.app(ks) 
-
   organization = uuid.uuid1()
-  print app.get('/sys') 
-
   ins = "INSERT INTO user (organization, username, password) VALUES (%s,%s,%s);"
   session.execute(ins, (organization, 'joe', bcrypt.hashpw('bcrypt', bcrypt.gensalt())))
   session.execute(ins, (organization, 'larry', bcrypt.hashpw('bcrypt', bcrypt.gensalt())))
   session.execute(ins, (organization, 'moe', bcrypt.hashpw('bcrypt', bcrypt.gensalt())))
-
-  print app.get('/sys') 
-  assert False
   rows = session.execute('SELECT username, password FROM user')
   d = [] 
   for r in rows:
     assert r.password == bcrypt.hashpw('bcrypt'.encode('utf-8'), r.password.encode('utf-8'))
 
 
+def test_login():
+  ks = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(16))
+  session = setup.keyspace(ks)
+  app = setup.app(ks) 
+  organization = uuid.uuid1()
+  ins = "INSERT INTO user (organization, username, password) VALUES (%s,%s,%s);"
+  session.execute(ins, (organization, 'joe', bcrypt.hashpw('password', bcrypt.gensalt())))
+  res = app.get('/auth/status') 
+  assert res.json.get('status') == ''
+
+  res = app.post('/auth/login', {'username':'joe','password':'wrongpass','organization':organization}) 
+  print res.json.get('message')
+  assert res.json.get('status') == 'ERROR'
+
+  res = app.post('/auth/login', {'username':'joe','password':'password','organization':organization}) 
+  print res.json.get('message')
+  assert res.json.get('status') == 'SUCCESSFUL'
+  assert res.json.get('key') == False
