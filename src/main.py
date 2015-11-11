f26d7aecae593fa96b7582b590b5520a268c5a1a
gavin myers <gavin@gavinm.com>
2015-11-10 21:40:07 -0500

    cleanup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from bottle import route, run, response
 from time import sleep
-import configparser, thread, requests, sys, bottle, api,appcontext
+import configparser, thread, requests, sys, bottle, appcontext, routes
 
 def init(ks,rdis):
   appcontext.keyspace = ks
--- src/routes.py
+++ src/routes.py
@@ -1,22 +1,10 @@
 from bottle import route,request 
 from cassandra.cluster import Cluster
 
-import uuid, collections, traceback, redis, thread, bcrypt, configparser, requests, sys, jsonpickle, random, os, zipfile, shutil
-
-import appcontext, organizationdao 
+import uuid, collections, traceback, thread, bcrypt, configparser, requests, sys, jsonpickle, random, os, zipfile, shutil, appcontext, auth, organizationdao 
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-
-def redis():
-  return appcontext.redis()
-
-def db():
-  return appcontext.db()
-
-syskey = config.get('application','syskey')
-def is_sys(key):
-  return key == syskey
 
 def callback(r, v):
     if r.query.get('callback') is not None:
@@ -24,26 +12,10 @@ def callback(r, v):
     else:
         return v
 
-def from_json(arg):
-  return jsonpickle.decode(arg)
-
-def to_json(arg):
-  return jsonpickle.encode(arg,unpicklable=False) 
-
-def session(key):
-  user = redis().get(key)
-  if user == None:
-    return None
-  redis().setex(key,2700,user)
-  organization_uid = uuid.UUID(key.split(":")[0])
-  #return key,user,organization_uid
-  return {'key':key,'user':user,'organization_uid':organization_uid}
-
-
 @route('/<key>/organization', method='POST')
 def admn_organization(key=""): 
   try:
-    if is_sys(key) == False:
+    if auth.is_sys(key) == False:
       return callback(res,{'status':'ERROR'})
 
     name = request.forms.get('name') 
@@ -57,8 +29,8 @@ def admn_organization(key=""):
 @route('/version')
 def sys_version():
   try:
-    redis().set('version','0.1.0')
-    return callback(request,{'version':redis().get('version')}) 
+    appcontext.redis().set('version','0.1.0')
+    return callback(request,{'version':appcontext.redis().get('version')}) 
   except Exception, e:
     exc_type, exc_value, exc_traceback = sys.exc_info()
     lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
@@ -74,11 +46,11 @@ def auth_login():
     if username is None or password is None:
       return callback(request,{'status':'INVALID'})
 
-    usr = db().execute('SELECT username, password, organization_uid from user where username=%s', (username,))[0]
+    usr = appcontext.db().execute('SELECT username, password, organization_uid from user where username=%s', (username,))[0]
     match = usr.password == bcrypt.hashpw(password.encode('utf-8'), usr.password.encode('utf-8')) and usr.organization_uid == organization_uid
     if match:
       key = "%s:%s" % (organization_uid, uuid.uuid1())
-      redis().setex(key,2700,username)
+      appcontext.redis().setex(key,2700,username)
       return callback(request,{'status':'OK', 'session':key})
     return callback(request,{'status':'ERROR'})
   except Exception, e:
@@ -89,7 +61,7 @@ def auth_login():
 @route('/<key>/auth/status')
 def auth_status(key=""):
   try:
-    s = session(key) 
+    s = auth.session(key) 
     if s == None:
       return callback(request,{'status':'ERROR'})
 
@@ -102,11 +74,11 @@ def auth_status(key=""):
 @route('/<key>/auth/logout')
 def auth_logout(key=""):
   try:
-    session = redis().get(key)
+    session = appcontext.redis().get(key)
     if session == None:
       return callback(request,{'status':'ERROR'})
 
-    session = redis().delete(key)
+    session = appcontext.redis().delete(key)
     return callback(request,{'status':'OK'})
   except Exception, e:
     exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -116,14 +88,14 @@ def auth_logout(key=""):
 @route('/<key>/users')
 def auth_status(key=""):
   try:
-    s = session(key) 
+    s = auth.session(key) 
     if s == None:
       return callback(request,{'status':'ERROR'})
 
     user = s.get('user')
     organization_uid = s.get('organization_uid')
 
-    rows = db().execute('SELECT user_username,organization_uid from user_by_organization where organization_uid=%s', (organization_uid,))
+    rows = appcontext.db().execute('SELECT user_username,organization_uid from user_by_organization where organization_uid=%s', (organization_uid,))
     d = []
     for r in rows:
       d.insert(0,{'username':r.user_username,'organization_uid':str(r.organization_uid)})
