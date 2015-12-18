584934dbaf4df367b893624c8c0feb844987185e
gavin myers <gavin@gavinm.com>
2015-12-18 18:18:14 -0500

    crud works now

--- test/test_interaction.py
+++ test/test_interaction.py
@@ -15,9 +15,9 @@ def test_insert():
   res = setup.app().post('/auth/login', {'username':'joe','password':'password','organization_uid':organization_uid}) 
   key = res.json.get('session')
 
-  setup.app().post('/%s/interaction', {'name':'interaction1','url':'url','organization_uid':organization_uid}) 
-  setup.app().post('/%s/interaction', {'name':'interaction2','url':'url','organization_uid':organization_uid}) 
-  setup.app().post('/%s/interaction', {'name':'interaction3','url':'url','organization_uid':organization_uid}) 
+  setup.app().post('/%s/interaction/create', {'name':'interaction1','url':'url','organization_uid':organization_uid}) 
+  setup.app().post('/%s/interaction/create', {'name':'interaction2','url':'url','organization_uid':organization_uid}) 
+  setup.app().post('/%s/interaction/create', {'name':'interaction3','url':'url','organization_uid':organization_uid}) 
 
   res = setup.app().get('/%s/interactions' % (key,) ) 
   print res.json.get('message')
@@ -36,19 +36,20 @@ def test_get_all_interactions():
   assert res.json.get('status') == 'OK' 
 
 def test_update_interaction():
-  res = setup.app().post('/%s/organization' % (syskey,), {'name':'testcorp', 'admin_username':'joe', 'admin_password':'password'})
+  res = setup.app().post('/%s/organization' % (syskey,), {'name':'testcorp', 'admin_username':'joe2', 'admin_password':'password'})
   organization_uid = str(res.json.get('uid'))
 
-  res = setup.app().post('/auth/login', {'username':'joe','password':'password','organization_uid':organization_uid}) 
+  res = setup.app().post('/auth/login', {'username':'joe2','password':'password'}) 
   key = res.json.get('session')
 
-  res = setup.app().post('/%s/interaction' % (key,), {'name':'interaction1','url':'url','organization_uid':organization_uid}) 
+  res = setup.app().post('/%s/interaction/create' % (key,), {'name':'interaction14','url':'url2'}) 
   interaction_uid = str(res.json.get('uid'))
-  print res.json.get('message')
-  print "interaction:" + interaction_uid
-  res = setup.app().post('/%s/interaction/%s' % (key,interaction_uid), {'url':'url2'})
-  assert res.json.get('status') == 'OK' 
-
-  res = setup.app().get('/%s/interaction/%s' % (key,interaction_uid) ) 
-  print res.json.get('message')
+  res = setup.app().get('/%s/interaction/view/%s' % (key,interaction_uid) ) 
+  print res.json.get('response')
   assert res.json.get('response').get('url') == 'url2'
+
+  res = setup.app().post('/%s/interaction/update/%s' % (key,interaction_uid), {'name':'interaction14','url':'mynewurl'})
+  print res.json.get('response')
+  res = setup.app().get('/%s/interaction/view/%s' % (key,interaction_uid)) 
+  print res.json.get('response')
+  assert res.json.get('response').get('url') == 'mynewurl'
